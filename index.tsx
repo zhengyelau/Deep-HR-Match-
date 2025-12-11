df3316a8a72957db8af2aaaad6bc0d915ff3bb87
zhengyelau <52906686+zhengyelau@users.noreply.github.com>
2025-12-11 13:44:23 +0800

    Updated package-lock.json

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import { jsxDEV } from "react/jsx-dev-runtime";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
